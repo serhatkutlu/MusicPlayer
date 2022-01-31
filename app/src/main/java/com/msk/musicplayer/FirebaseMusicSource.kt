7a28fea49b41844efc48e603fcf16748e7a4903b
serhatkutlu <84921644+serhatkutlu@users.noreply.github.com>
2022-01-31 14:59:21 +0300

    This class performs a state check when receiving data.

---
+++ app/src/main/java/com/msk/musicplayer/FirebaseMusicSource.kt
@@ -0,0 +1,35 @@
+package com.msk.musicplayer
+
+class FirebaseMusicSource {
+    private val onReadyListeners= mutableListOf<(Boolean)->Unit>()
+
+    private var state:State=State.STATE_CREATED
+    set(value) {
+        if (value==State.STATE_INITIALIZED||value ==State.STATE_ERROR){
+            synchronized(onReadyListeners){
+                field=value
+                onReadyListeners.forEach{
+                    it(state==State.STATE_INITIALIZED)
+                }
+            }
+        }else{
+            field=value
+        }
+    }
+    fun whenReady(action:(Boolean) -> Unit):Boolean{
+        if (state==State.STATE_CREATED||state==State.STATE_INITIALIZING){
+            onReadyListeners+=action
+            return false
+        }else{
+            action(state==State.STATE_INITIALIZED)
+            return true
+        }
+    }
+}
+
+enum class State{
+    STATE_CREATED,
+    STATE_INITIALIZING,
+    STATE_INITIALIZED,
+    STATE_ERROR,
+}
